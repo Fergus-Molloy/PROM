cbeba048caf81ae8ad4f1eaa2dbbae6b741e2926
FD Molloy <fdm504@cse169pc-59.cs.york.ac.uk>
2019-04-18 13:27:16 +0100

    Lab 2 work

--- pong.py
+++ pong.py
@@ -1,29 +1,42 @@
-import pygame, sys
-from pygame.locals import *
+import RPi.GPIO as GPIO
+import time, sys
+from serial import Serial
+from constants import constant as c
 
-#Number of frames per second
-FPS = 200
-#size of the window
-WINDOW_WIDTH  = 400
-WINDOW_HIEGHT = 200
+def clear(serialPort):
+	serialPort.write(chr(27)+"[0;0H")
+	for y in range(c.WINDOW_HEIGHT):
+#		for x in range(c.WINDOW_WIDTH):
+		serialPort.write(chr(27)+"[40m ")
+		serialPort.write(chr(27)+"[1B")
+		serialPort.write(chr(27)+"[1D")
+		print y
+	serialPort.write(chr(27)+"[0;0H")
+
+def drawInit(serialPort):
+	batHeight = c.WINDOW_HEIGHT/2 - c.BAT_SIZE
+	string = "[%d;0H" %(batHeight,)
+	serialPort.write(chr(27)+string)
+	string = chr(27) + "[44m "
+	for x in range(c.BAT_SIZE):
+		serialPort.write(string)
+		serialPort.write(chr(27)+"[1B")
+		serialPort.write(chr(27)+"[1D")
 
 def main():
-	pygame.init()
-	#creates a global surface for pygame to draw on
-	global DISPLAYSURF
-	#allows to specify how fast the program runs
-	FPSCLOCK = pygame.time.Clock()
-	DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOWHEIGHT))
-	pygame.display.set_caption("Pong")
+	serialPort = Serial("/dev/ttyAMA0", 9600)
+	
+	if serialPort.isOpen() == False:
+		serialPort.open()
 
-	while True: #main game loop
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				pygame.quit()
-				sys.exit()
-
-		pyagame.display.update()
-		FPSCLOCK.tick(FPS)
+	drawInit(serialPort)
+	go = True
+	while go:
+		inputString = serialPort.read()
+		if ord(inputString) == 112:
+			go = False
+			clear(serialPort)
+			
 
 if __name__ == "__main__":
 	main()
