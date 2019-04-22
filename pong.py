ef00e0d7ada9df68aedf36c0c226b83cdcfd8fcc
Fergus Molloy <fdm504@york.ac.uk>
2019-04-22 13:00:12 +0100

    created functions to allow testing at home

--- pong.py
+++ pong.py
@@ -1,42 +1,50 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+import readchar
 import time, sys
-from serial import Serial
+#from serial import Serial
 from constants import constant as c
 
-def clear(serialPort):
-	serialPort.write(chr(27)+"[0;0H")
-	for y in range(c.WINDOW_HEIGHT):
-#		for x in range(c.WINDOW_WIDTH):
-		serialPort.write(chr(27)+"[40m ")
-		serialPort.write(chr(27)+"[1B")
-		serialPort.write(chr(27)+"[1D")
-		print y
-	serialPort.write(chr(27)+"[0;0H")
+def write(string):
+	sys.stdout.write(str(string))
 
-def drawInit(serialPort):
-	batHeight = c.WINDOW_HEIGHT/2 - c.BAT_SIZE
-	string = "[%d;0H" %(batHeight,)
-	serialPort.write(chr(27)+string)
-	string = chr(27) + "[44m "
-	for x in range(c.BAT_SIZE):
-		serialPort.write(string)
-		serialPort.write(chr(27)+"[1B")
-		serialPort.write(chr(27)+"[1D")
-
-def main():
+def serialSetup():
 	serialPort = Serial("/dev/ttyAMA0", 9600)
-	
 	if serialPort.isOpen() == False:
 		serialPort.open()
+	return serialPort
 
-	drawInit(serialPort)
+def clear():
+	write(chr(27)+"[0;0H")
+	for y in range(c.WINDOW_HEIGHT):
+#		for x in range(c.WINDOW_WIDTH):
+		write(chr(27)+"[40m ")
+		write(chr(27)+"[1B")
+		write(chr(27)+"[1D")
+		print y
+	write(chr(27)+"[0;0H")
+
+def drawInit():
+	batHeight = c.WINDOW_HEIGHT/2 - c.BAT_SIZE
+	string = "[%d;0H" %(batHeight,)
+	write(chr(27)+string)
+	string = chr(27) + "[44m "
+	for x in range(c.BAT_SIZE):
+		write(string)
+		write(chr(27)+"[1B")
+		write(chr(27)+"[1D")
+
+def main():
+#	serailPort = serialSetup()
+
+	drawInit()
 	go = True
 	while go:
-		inputString = serialPort.read()
+#		inputString = serialPort.read()
+		inputString = readchar.readchar()
 		if ord(inputString) == 112:
 			go = False
-			clear(serialPort)
-			
+			clear()
+
 
 if __name__ == "__main__":
 	main()
