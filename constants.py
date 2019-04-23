3ccf8ec11826b41cc3a29581434eafa24805e755
Fergus Molloy <fdm504@york.ac.uk>
2019-04-23 12:21:14 +0100

    finished numbers and score writing

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 class constant:
-	WINDOW_HEIGHT=20
+	WINDOW_HEIGHT=24
 	WINDOW_WIDTH=80
 	BAT_SIZE=3
 	DIFF_TRAJECTORY=False
@@ -17,3 +17,65 @@ class constant:
 	colorMagenta = chr(27)+"[45m "
 	colorCyan    = chr(27)+"[46m "
 	colorWhite   = chr(27)+"[47m "
+
+	#numbers
+	zero =[
+	esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D"]
+	one =[
+	esc+"[45m "+esc+"[45m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[45m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[45m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[45m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D"]
+	two =[
+	esc+"[45m "+esc+"[45m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[45m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D"]
+	three =[
+	esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D"]
+	four =[
+	esc+"[45m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D"]
+	five =[
+	esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[40m "+esc+"[1B"+esc+"[3D"]
+	six =[
+	esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[40m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D"]
+	seven =[
+	esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D"]
+	eight =[
+	esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D"]
+	nine =[
+	esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[45m "+esc+"[45m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D",
+        esc+"[40m "+esc+"[40m "+esc+"[45m "+esc+"[1B"+esc+"[3D"]
--- pong.py
+++ pong.py
@@ -1,14 +1,16 @@
-#import RPi.GPIO as GPIO
-import readchar
+import RPi.GPIO as GPIO
+#import readchar
 import time, sys
-#from serial import Serial
+from serial import Serial
 from constants import constant as c
 
 def write(string):
-	sys.stdout.write(str(string))
+#	sys.stdout.write(str(string))
+	serialPort.write(str(string))
 
 def serialSetup():
-	serialPort = Serial("/dev/ttyAMA0", 9600)
+	global serialPort
+	serialPort = Serial("/dev/ttyAMA0", 115200)
 	if serialPort.isOpen() == False:
 		serialPort.open()
 	return serialPort
@@ -25,52 +27,95 @@ def clear():
 #-----Draw Bats-----
 def drawLeftBat(batY):
 	write(c.esc+"[0;0H")
-	for x in range(batY,c.BAT_SIZE):
-		bat = "[%d;0H" %(x,)
+	for y in range(batY, batY+c.BAT_SIZE):
+		bat = "[%d;0H" %(y,)
 		write(c.esc+bat)
 		write(c.colorBlue)
 
 def drawRightBat(batY):
 	write(c.esc+"[0;0H")
-	for x in range(c.BAT_SIZE):
-		batHeight = (c.WINDOW_HEIGHT - c.BAT_SIZE)/2 + x
+	for x in range(batY, batY+c.BAT_SIZE):
 		bat = "[%d;%dH" %(x, c.WINDOW_WIDTH)
 		write(c.esc+bat)
 		write(c.colorBlue)
 
-def drawBats(pos)
+def drawBats(pos):
 	drawLeftBat(pos)
 	drawRightBat(pos)
 
 #-----Draw Center-----
 def drawCenter():
+	b=0 #for 2 bit counter
 	for x in range(c.WINDOW_HEIGHT):
 		middle = "[%d;%dH" %(x, c.WINDOW_WIDTH/2)
 		write(c.esc+middle)
-		if x%4 == 0:
-			write(c.esc+color1)
+		if b == 3: #2 bits are full
+			b = 0 #"overflow"
+			write(c.colorYellow)
+		elif b < 2:
+			b += 1
+			write(c.colorBlack)
 		else:
-			write(c.esc+color2)
+			b += 1
+			write(c.colorYellow)
+
+#-----Draw Scores-----
+def scoreSequence(score):
+	scores =[
+	c.zero,
+	c.one,
+	c.two,
+	c.three,
+	c.four,
+	c.five,
+	c.six,
+	c.seven,
+	c.eight,
+	c.nine
+	]
+	return scores[score]
+
+def drawLeftScore(score):
+	xOffset = c.WINDOW_WIDTH / 4
+	yOffset = c.WINDOW_HEIGHT / 10
+	string = scoreSequence(score)
+	write(chr(27)+"[%d;%dH"%(yOffset,xOffset))
+	for x in string:
+		write(x)
+
+def drawRightScore(score):
+	xOffset =c.WINDOW_WIDTH - (c.WINDOW_WIDTH/4)
+        yOffset = c.WINDOW_HEIGHT / 10
+        string = scoreSequence(score)
+        write(chr(27)+"[%d;%dH"%(yOffset,xOffset))
+        for x in string:
+                write(x)
+
+def drawScores(score):
+	drawLeftScore(score)
+	drawRightScore(score)
 
 #-----Draw Initial-----
 def drawInit():
 	clear()
 	middle = (c.WINDOW_HEIGHT - c.BAT_SIZE)/2
 	drawBats(middle)
+	drawCenter()
+	drawScores(9)
 
 
 #----------Main----------
 def main():
-#	serailPort = serialSetup()
+	serailPort = serialSetup()
 
 	drawInit()
 	go = True
-	while go:
+#	while go:
 #		inputString = serialPort.read()
-		inputString = readchar.readchar()
-		if ord(inputString) == 112:
-			go = False
-			clear()
+#		inputString = readchar.readchar()
+#		if ord(inputString) == 112:
+#			go = False
+#			clear()
 
 
 if __name__ == "__main__":
